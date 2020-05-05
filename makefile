696efb3bc9f98a5973c25139c156b128d6ba318c
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-05-05 16:03:11 -0400

    styles go in public

--- makefile
+++ makefile
@@ -1,3 +1,2 @@
 sass-comp:
-	sass scss/bootstrap.scss src/bootstrap.css
 	sass scss/bootstrap.scss public/css/bootstrap.css
--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@
 */
 
 import React from "react";
-import "./App.css";
 
 class App extends React.Component {
     render() {
