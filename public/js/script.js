11d147f2036fbb670b9c628d34720014ef85ceea
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-05-06 20:03:17 -0400

    added colour change for navbar

---
+++ public/js/script.js
@@ -0,0 +1,37 @@
+/*
+    Carriage Crossing Pharmacy Booker
+    Copyright (C) 2020 Luke Zhang, Ethan Lim
+
+    https://luke-zhang-04.github.io/
+    https://github.com/ethanlim04
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+window.onscroll = () => {
+    scroll()
+}
+
+// Add a background to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function scroll() {
+    var navbar = document.querySelector(".navbar")
+    if (window.pageYOffset >= 100) {
+        navbar.classList.remove("bg-default")
+        navbar.classList.remove("bg-none")
+        navbar.classList.add("bg-light")
+    } else {
+        navbar.classList.remove("bg-light")
+        navbar.classList.add("bg-none")
+    }
+}
