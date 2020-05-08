becbbaa95ce333ac44531ceb41d9c7dc9016c074
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-05-08 14:41:39 -0400

    added test

--- src/App.test.js
+++ src/App.test.js
@@ -18,9 +18,14 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-
+import React from 'react';
+import renderer from 'react-test-renderer';
 import App from "./App";
 
 test("test should give no errors", () => {
-    app = App.render();
+    const component = renderer.create(
+        <App/>
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
 })
