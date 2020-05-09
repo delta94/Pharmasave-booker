93d54374cc242c496740813ba6760d3e20a0254a
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-05-09 10:00:40 -0400

    added reg form

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -44,32 +44,118 @@ class GoogleSignInButton extends React.Component {
     }
 }
 
-class Login extends React.Component {
+class Login extends React.Component<any, any> {
+    constructor(props: object) {
+        super(props)
+        this.state = {
+            display: "block"
+        }
+    }
+    
+    private form_components = {
+        email: (
+            <label>
+                Email
+                <input type="email" className="form-control" id="login-email" aria-describedby="emailHelp" placeholder="Enter email"/>
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </label>
+        ),
+        password: (
+            <label>
+                Password
+                <input type="password" className="form-control" id="login-password" placeholder="Password"/>
+            </label>
+        ),
+        buttons: (
+            <div className="row">
+                <div className="col-6">
+                    <button type="submit" className="btn btn-secondary">Login</button>  
+                </div>
+                <div className="col-6">
+                    <GoogleSignInButton/>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
-            <form>
+            <form style={{display: this.state.display}}>
                 <div className="form-group">
-                    <label>
-                        Email
-                        <input type="email" className="form-control" id="login-email" aria-describedby="emailHelp" placeholder="Enter email"/>
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </label>
+                    {this.form_components.email}
                 </div>
                 <div className="form-group">
-                    <label>
-                        Password
-                        <input type="password" className="form-control" id="login-password" placeholder="Password"/>
-                    </label>
+                    {this.form_components.password}
                 </div>
                 <a href="#">Forgot your password?</a>
-                <div className="row">
-                    <div className="col-6">
-                        <button type="submit" className="btn btn-secondary">Login</button>  
-                    </div>
-                    <div className="col-6">
-                        <GoogleSignInButton/>
-                    </div>
+                {this.form_components.buttons}
+            </form>
+        );
+    }
+}
+
+class Reg extends React.Component<any, any> {
+    constructor(props: object) {
+        super(props)
+        this.state = {
+            display: "none"
+        }
+    }
+
+    private form_components = {
+        name: (
+            <label>
+                Full, legal name
+                <input type="name" className="form-control" id="reg-name" aria-describedby="emailHelp" placeholder="Full, legal name"/>
+            </label>
+        ),
+        email: (
+            <label>
+                Email
+                <input type="email" className="form-control" id="reg-email" aria-describedby="emailHelp" placeholder="Email"/>
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </label>
+        ),
+        password: (
+            <label>
+                Password
+                <input type="password" className="form-control" id="reg-password" placeholder="Password"/>
+            </label>
+        ),
+        password2: (
+            <label>
+                Confirm you Password
+                <input type="password" className="form-control" id="reg-password-2" placeholder="Retype password"/>
+            </label>
+        ),
+        buttons: (
+            <div className="row">
+                <div className="col-6">
+                    <button type="submit" className="btn btn-secondary">Register</button>  
                 </div>
+                <div className="col-6">
+                    <GoogleSignInButton/>
+                </div>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <form style={{display: this.state.display}}>
+                <div className="form-group">
+                    {this.form_components.name}
+                </div>
+                <div className="form-group">
+                    {this.form_components.email}
+                </div>
+                <div className="form-group">
+                    {this.form_components.password}
+                </div>
+                <div className="form-group">
+                    {this.form_components.password2}
+                </div>
+                {this.form_components.buttons}
             </form>
         );
     }
@@ -79,10 +165,13 @@ class Auth extends React.Component {
     render() {
         return (
             <div className="auth-container container">
-                <div className="back-container"></div>
+                <div className="back-container">
+
+                </div>
                 <div className="form-container">
                     <h1 className="container">Login</h1>
                     <Login/>
+                    <Reg/>
                 </div>
             </div>
         );
