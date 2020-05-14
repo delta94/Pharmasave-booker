7b01c918db29301d45919f2836a8d74afbced87b
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-05-14 18:33:03 -0400

    style(index.js) adheres to eslint rules
    line 31: disable eslint
    line 35: docstring instead of comments

--- src/index.js
+++ src/index.js
@@ -28,10 +28,13 @@ ReactDOM.render(
     <React.StrictMode>
         <App/>
     </React.StrictMode>,
-    document.getElementById("root"),
+    // eslint-disable-next-line
+    document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+/**
+ * If you want your app to work offline and load faster, you can change
+ * unregister() to register() below. Note this comes with some pitfalls.
+ * Learn more about service workers: https://bit.ly/CRA-PWA
+ */
 serviceWorker.unregister();
