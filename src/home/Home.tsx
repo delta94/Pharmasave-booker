3ee685173287894fd63f5237b744a9155103c382
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-05-14 10:43:26 -0400

    docs: add contributing

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -51,7 +51,7 @@ class Home extends React.Component {
         )
     }
     
-    constructor(props: any) {
+    constructor(props: {}) {
         super(props);
         this.state = {
             imageLoaded: false
