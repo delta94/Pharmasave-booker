63535b1c15f0c2e75ba4f1a1ad5810bb43c837d5
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-05-14 14:47:33 -0400

    style(Auth.js): adhere to esline rules
    line 28: remove braces from argument
    line 33: add newline after argument

--- src/Auth.js
+++ src/Auth.js
@@ -24,12 +24,14 @@ import firebaseApp from "./firebase";
 
 export const AuthContext = React.createContext()
 
-export const AuthProvider = ({children}) => {
+// eslint-disable-next-line
+export const AuthProvider = (children) => {
     const [currentUser, setCurrentUser] = React.useState(null)
 
     React.useEffect(() => {
         firebaseApp.auth().onAuthStateChanged(setCurrentUser)
-    }, [])
+    },
+    [])
 
     return (
         <AuthContext.Provider
