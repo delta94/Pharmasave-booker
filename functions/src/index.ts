d116deecb40c78d29e0f3fd0055dd7e34fe2a411
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-05-27 12:40:04 -0400

    remove functions boilerplate

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,20 @@
-// import * as functions from 'firebase-functions';
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-console.log("WTF")
+/**
+ * Carriage Crossing Pharmacy Booker
+ * Copyright (C) 2020 Luke Zhang, Ethan Lim
+ * 
+ * https://luke-zhang-04.github.io/
+ * https://github.com/ethanlim04
+ * This program is free software: you can redistribute it and/or modif
+ * it under the terms of the GNU General Public License as published b
+ * the Free Software Foundation, either version 3 of the License, o
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be usefu
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See t
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public Licen
+ * 
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
