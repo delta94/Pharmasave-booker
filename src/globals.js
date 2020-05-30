10d88efd80d9dce7d96c64bd1323d360678cecb4
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-05-29 23:56:05 -0400

    feat(client): change globals.json to globals.js
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

---
+++ src/globals.js
@@ -0,0 +1,24 @@
+/**
+ * Carriage Crossing Pharmacy Booker
+ * Copyright (C) 2020 Luke Zhang, Ethan Lim
+ * 
+ * https://luke-zhang-04.github.io/
+ * https://github.com/ethanlim04
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public Licence
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+export default globals = {
+    
+}
